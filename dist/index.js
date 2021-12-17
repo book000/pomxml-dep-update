6dd4cf5f21c6e60d1bb85c3aaa87694f16d91f26
Tomachi [ICHIGO] <lego@aol.jp>
2021-12-17 21:41:07 +0900

    fix: import core -> import * as core

--- dist/index.js
+++ dist/index.js
@@ -8105,6 +8105,25 @@ module.exports = parse;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8118,7 +8137,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
+const core = __importStar(__nccwpck_require__(2186));
 const axios_1 = __importDefault(__nccwpck_require__(6545));
 const fast_xml_parser_1 = __nccwpck_require__(2603);
 const fs_1 = __importDefault(__nccwpck_require__(7147));
@@ -8289,8 +8308,8 @@ function main(pomPath, ignorePackages) {
 }
 ;
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    const pomPath = core_1.default.getInput('pom-path');
-    const ignorePackages = core_1.default.getInput('ignore-packages');
+    const pomPath = core.getInput('pom-path');
+    const ignorePackages = core.getInput('ignore-packages');
     main(pomPath, ignorePackages);
 }))();
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core'
+import * as core from '@actions/core'
 import axios from 'axios'
 import { XMLBuilder, XMLParser } from 'fast-xml-parser'
 import fs from 'fs'
